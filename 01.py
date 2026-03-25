3d02ca3d397f5b0cabca070957f67e539cd9f2be
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-25 23:52:59 +0530

    dsa

---
+++ 01.py
@@ -0,0 +1,10 @@
+'''1. Reverse a String'''
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("aditya "))
+
+def asr(a):
+    return a[::-1]
+print(reverse_string("rajput"))
+
---
+++ 02.py
@@ -0,0 +1,9 @@
+'''2. Check Palindrome'''
+
+def is_palindrome(p):
+    return p==p[::-1]
+print(is_palindrome("madam"))
+
+def yeeh_boy(a):
+    return a==a[::-1]
+print(is_palindrome("jii"))
