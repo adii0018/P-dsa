a51c5a679c5b5d0ce6cdb53e40079f6f2302dd85
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-27 23:36:01 +0530

    06

---
+++ 06.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def word_ladder(beginWord, endWord, wordList):
+    wordSet = set(wordList)
+    queue = deque([(beginWord, 1)])
+
+    while queue:
+        word, steps = queue.popleft()
+
+        if word == endWord:
+            return steps
+
+        for i in range(len(word)):
+            for c in "abcdefghijklmnopqrstuvwxyz":
+                new_word = word[:i] + c + word[i+1:]
+                if new_word in wordSet:
+                    queue.append((new_word, steps + 1))
+                    wordSet.remove(new_word)
+
+    return 0
